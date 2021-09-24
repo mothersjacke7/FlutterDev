63803902257e173c7e0c5007acebbb953abb6c8f
mothersjacke7 <makarikof@ya.ru>
2021-09-24 11:42:46 +0300

    refactoring main.dart adds russian titles

--- flutter_project1/lib/main.dart
+++ flutter_project1/lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Онлайн рандомизер даблг млг'),
     );
   }
 }
