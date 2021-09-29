d751fcdb4672396adbb3a03e047e4814eb315677
mothersjacke7 <makarikof@ya.ru>
2021-09-29 18:52:22 +0300

    Refactor: added fonts text

--- flutter_project1/lib/main.dart
+++ flutter_project1/lib/main.dart
@@ -52,7 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Рандомное число, берущееся с random api',
+              'randomizer',
+              style: TextStyle(fontStyle: FontStyle.italic),
             ),
             Counter(_value),
           ],
